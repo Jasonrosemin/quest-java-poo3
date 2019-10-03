d1fa2675f0c4ce1172164d631074a221a294a8dd
rosemin <jasonliverpool@hotmail.fr>
2019-10-03 19:27:28 +0200

    quete modifiée

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -20,5 +20,23 @@ public class Eagle extends Bird {
     @Override
     public String sing() {
         return "Screech!";
+    }
+    public void takeOff() {
+    	System.out.println("taking off !");
+    }
+    public int ascend (int meters) {
+    	System.out.println("ascend : 120 meters !");
+    	return 120;
+    	
+    }
+    @Override
+    public int descend (int meters) {
+    	System.out.println("descend : 20 meters !");
+    	return 20;
+    	
+    }
+   
+    public void land () {
+    	System.out.println("Landing !");
     }
 }
--- Fly.java
+++ Fly.java
@@ -1,4 +1,16 @@
 
 public interface Fly {
+	
+	void takeOff();
+	
+	public int ascend (int meters);
+	
+	public int descend (int meters);
 
+  public 	void land ();
+	
+	default void glide () {
+		
+		System.out.println("It glides into the air.");
+	}
 }
